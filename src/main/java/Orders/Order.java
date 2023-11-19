18ba6b9328332090c5a6e978bdcb806f9923ad8f
Desmond <desmonds534@gmail.com>
2023-11-20 00:22:05 +0800

    update Order constructor for mods

--- src/main/java/Orders/Order.java
+++ src/main/java/Orders/Order.java
@@ -4,22 +4,20 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Order {
-    @Getter private long timestamp;
-    @Getter private int orderId;
+    @Getter @Setter private long timestamp;
+    @Getter @Setter private int orderId;
     @Getter private int securityId;
     @Getter private Side side;
     @Getter @Setter private int quantity; // setter for quantity matching
     @Getter private Double price;
-    @Getter private Limit parentLimit;
+    @Getter @Setter private Limit parentLimit;
     @Getter private ORDER_TYPE ordType = ORDER_TYPE.MARKET;
     @Getter private boolean isBuy;
 
     @Getter @Setter private Order nextOrder;
     @Getter @Setter private Order prevOrder;
 
-    public Order(long timestamp, int orderId, int securityId, Side side, int quantity, Double price){
-        this.timestamp = timestamp;
-        this.orderId = orderId;
+    public Order(int securityId, Side side, int quantity, Double price){
         this.securityId = securityId;
         this.side = side;
         this.quantity = quantity;
@@ -31,14 +29,30 @@ public class Order {
         }
         this.isBuy = side == Side.BUY;
     }
-    public Order(Order other, int orderId)
+    public Order(Order other, double price)
     {
-        this.timestamp = other.timestamp;
-        this.orderId = orderId;
         this.securityId = other.securityId;
         this.side = other.side;
+        this.isBuy = other.isBuy;
         this.quantity = other.quantity;
+        this.price = price;
+        this.parentLimit = new Limit(price);
+        this.ordType = ORDER_TYPE.LIMIT;
+    }
+
+    public Order(Order other, int quantity)
+    {
+        this.securityId = other.securityId;
+        this.side = other.side;
+        this.isBuy = other.isBuy;
+        this.quantity = quantity;
         this.price = other.price;
         this.parentLimit = other.parentLimit;
+        this.ordType = ORDER_TYPE.LIMIT;
+    }
+
+    @Override
+    public String toString() {
+        return "Order(orderId=" + orderId + ", timestamp=" + timestamp + ", price=" + price + ", qty=" + quantity + ", nextOrder=" + nextOrder + ")";
     }
 }
