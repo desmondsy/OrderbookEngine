b09cfcffaeb3c8578a3e1632722a8964c84a6925
desmond <desmond.sy@itarle.com>
2023-11-20 10:55:01 +0000

    update market order matching logic + add aggressive limit matching logic

--- src/main/java/MatchingEngine/PriceTimePriorityMatcher.java
+++ src/main/java/MatchingEngine/PriceTimePriorityMatcher.java
@@ -28,25 +28,29 @@ class PriceTimePriorityMatcher implements IOrderMatcher {
             // only iterate limit levels that have at least one order in it.
             // e.g. if we are 100/101 and then we deplete 101 with a market order, the 101 limit is technically still here
             // but will be null (ptr = null). So we don't do any matching on that null level
-            while(ptr!=null)
+            while(ptr!=null && o.getQuantity() > 0)
             {
                 if (o.getQuantity() >= ptr.getQuantity())
                 {
-                    trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), o.getQuantity(), ptr.getOrderId(), o.getOrderId()));
+                    trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), ptr.getQuantity(), ptr.getOrderId(), o.getOrderId()));
                     o.setQuantity(o.getQuantity() - ptr.getQuantity());
                     ob.removeOrder(ptr.getOrderId());
                     ptr = ptr.getNextOrder();
                 }
                 else
                 {
-                    trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), ptr.getQuantity(), ptr.getOrderId(), o.getOrderId()));
-                    o.setQuantity(o.getQuantity() - ptr.getQuantity());
-                    ob.removeOrder(ptr.getOrderId());
-                    if (o.getQuantity() <= 0)
-                    {
-                        break;
-                    }
+                    trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), o.getQuantity(), ptr.getOrderId(), o.getOrderId()));
+                    ptr.setQuantity(ptr.getQuantity() - o.getQuantity());
+                    o.setQuantity(0);
+                    if (ptr.getQuantity() == 0)
+                        ob.removeOrder(ptr.getOrderId());
                 }
+            }
+
+            if (o.getQuantity() <= 0)
+            {
+                ob.removeOrder(o.getOrderId());
+                break;
             }
 
             // if we reach here, we have depleted the current limit level. E.g order for 1000 but best ask is < 1000.
@@ -65,6 +69,48 @@ class PriceTimePriorityMatcher implements IOrderMatcher {
     @Override
     public void matchAggressiveLimitOrder(Order o, Orderbook ob)
     {
+        int totalSize = o.isBuy() ? ob.getTotalAskSize() : ob.getTotalBidSize();
+        if (o.getQuantity() >= totalSize)
+        {
+            // if e.g. total ask size is 10000, but an aggressive buy order comes in for 20000, we aren't going to match it.
+            return;
+        }
 
+        TreeSet<Limit> limitTree = o.isBuy() ? ob.getAskLimits() : ob.getBidLimits();
+        double farTouchPrice = o.isBuy() ? ob.getBestAsk() : ob.getBestBid();
+
+        for (Limit limit: limitTree)
+        {
+            if (limit.getPrice() == farTouchPrice)
+            {
+                Order ptr = limit.getHead();
+
+                while(ptr!=null)
+                {
+                    if (o.getQuantity() >= ptr.getQuantity())
+                    {
+                        trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), o.getQuantity(), ptr.getOrderId(), o.getOrderId()));
+                        o.setQuantity(o.getQuantity() - ptr.getQuantity());
+                        ob.removeOrder(ptr.getOrderId());
+                        ptr = ptr.getNextOrder();
+                    }
+                    else
+                    {
+                        trades.add(new Trade(o.getSide(), ptr.getParentLimit().getPrice(), ptr.getQuantity(), ptr.getOrderId(), o.getOrderId()));
+                        o.setQuantity(o.getQuantity() - ptr.getQuantity());
+                        ob.removeOrder(ptr.getOrderId());
+                        if (o.getQuantity() <= 0)
+                        {
+                            return;
+                        }
+                    }
+                }
+                break;
+            }
+
+            // e.g. if we do an aggressive limit buy 500 qty on a book like 500 x 100/101 x 200, any remaining qty
+            ob.addOrder(new Order(o.getSecurityId(), o.getSide(), o.getQuantity(), farTouchPrice));
+
+        }
     }
 }
