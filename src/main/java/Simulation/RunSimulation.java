58aaa489ffaecbd5b1a5909adbf7dc8a1475b4e0
desmond <desmond.sy@itarle.com>
2023-12-25 11:09:40 +0800

    add RunSimulation class as the driver program

--- src/main/java/Simulation/RunSimulation.java
+++ src/main/java/Simulation/RunSimulation.java
@@ -6,7 +6,7 @@ import Orderbook.Orderbook;
 import java.io.IOException;
 
 public class RunSimulation {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SimulationConfig simulationConfig = new SimulationConfig("simulationConfig.properties");
         Orderbook ob = new Orderbook(OrderMatcherFactory.createOrderMatcher(simulationConfig.getMatchingEngine()));
         OrderbookSimulator simulator = new OrderbookSimulator(ob, simulationConfig);
