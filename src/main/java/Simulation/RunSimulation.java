a51909ef63431c7454076b25d11212d66963a42e
desmondsy <desmonds534@gmail.com>
2023-12-29 22:51:58 +0800

    update RunSimulation args

--- src/main/java/Simulation/RunSimulation.java
+++ src/main/java/Simulation/RunSimulation.java
@@ -7,8 +7,27 @@ import java.util.Random;
 
 public class RunSimulation {
     public static void main(String[] args) {
-        Random random = new Random(123);
-        SimulationConfig simulationConfig = new SimulationConfig("simulationConfig.properties");
+        int seed;
+        String propertiesFile;
+
+        try {
+            propertiesFile = args[0]; // in src/main/resources
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            propertiesFile = "simulationConfig.properties";
+        }
+
+        try {
+            seed = Integer.parseInt(args[1]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            seed = 123;
+        }
+
+        Random random = new Random(seed);
+        SimulationConfig simulationConfig = new SimulationConfig(propertiesFile);
         Orderbook ob = new Orderbook(OrderMatcherFactory.createOrderMatcher(simulationConfig.getMatchingEngine()), random);
         OrderbookSimulator simulator = new OrderbookSimulator(ob, simulationConfig, random);
 
